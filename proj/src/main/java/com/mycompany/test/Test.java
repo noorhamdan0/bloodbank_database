918e1c8d2e49cacc93d7d8b416488503bcd8cd60
noor hamdan <124586788+noorhamdan0@users.noreply.github.com>
2023-12-17 22:20:38 +0200

    PROJ

---
+++ proj/src/main/java/com/mycompany/test/Test.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.test;
+
+/**
+ *
+ * @author Amjad
+ */
+public class Test {
+
+    public static void main(String[] args) {
+      //new Log_in().setVisible(true);
+          new Enter().setVisible(true);
+          // new forgetpassword().setVisible(true);
+         // new RegistrationForm1().setVisible(true);
+        // new RF2().setVisible(true);
+          //new RF3().setVisible(true);
+          //new doctor().setVisible(true);
+       // new donor().setVisible(true);
+         
+    }
+}
